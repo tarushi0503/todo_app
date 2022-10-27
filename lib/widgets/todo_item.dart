a3856fb9da39bd5f997644a9dc2e829dc71bcb5b
Yash Bhardwaj <yashbhardwaj21072000@gmail.com>
2022-10-27 22:42:39 +0530

    All errors removed

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -3,7 +3,9 @@ import '../model/todo.dart';
 
 class ToDoItem extends StatelessWidget {
   final ToDo todo;
+  // ignore: prefer_typing_uninitialized_variables
   final onToDoChanged;
+  // ignore: prefer_typing_uninitialized_variables
   final onDeleteItem;
 
   const ToDoItem(
@@ -16,14 +18,14 @@ class ToDoItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.only(bottom: 20),
+        margin: const EdgeInsets.only(bottom: 20),
         child: ListTile(
           onTap: () {
             onToDoChanged(todo);
           },
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
           tileColor: Colors.white70,
           leading: Icon(
             todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
@@ -33,13 +35,13 @@ class ToDoItem extends StatelessWidget {
             todo.todoText!,
             style: TextStyle(
               fontSize: 16,
-              color: Color.fromARGB(255, 60, 30, 64),
+              color: const Color.fromARGB(255, 60, 30, 64),
               decoration: todo.isDone ? TextDecoration.lineThrough : null,
             ),
           ),
           trailing: Container(
-            padding: EdgeInsets.all(0),
-            margin: EdgeInsets.symmetric(vertical: 12),
+            padding: const EdgeInsets.all(0),
+            margin: const EdgeInsets.symmetric(vertical: 12),
             height: 35,
             width: 35,
             decoration: BoxDecoration(
@@ -49,7 +51,7 @@ class ToDoItem extends StatelessWidget {
             child: IconButton(
               color: Colors.white,
               iconSize: 18,
-              icon: Icon(Icons.delete),
+              icon: const Icon(Icons.delete),
               onPressed: () {
                 onDeleteItem(todo.id);
               },
